c3b688fd9972dfe5b3baa9df6b6246d320737d90
abdelwaheb-net <abdelwaheb_ammar@hotmail.fr>
2020-10-18 16:10:14 +0100

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnstratgame = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(138, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 309);
+            this.panel1.TabIndex = 0;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(183, 94);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 90);
+            this.button6.TabIndex = 0;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(182, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 90);
+            this.button3.TabIndex = 0;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(93, 94);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 90);
+            this.button5.TabIndex = 0;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 90);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnstratgame
+            // 
+            this.btnstratgame.Location = new System.Drawing.Point(144, 337);
+            this.btnstratgame.Name = "btnstratgame";
+            this.btnstratgame.Size = new System.Drawing.Size(263, 42);
+            this.btnstratgame.TabIndex = 1;
+            this.btnstratgame.Text = "Start Game";
+            this.btnstratgame.UseVisualStyleBackColor = true;
+            this.btnstratgame.Click += new System.EventHandler(this.btnstratgame_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(2, 94);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(90, 90);
+            this.button11.TabIndex = 0;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 185);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(90, 90);
+            this.button7.TabIndex = 0;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(92, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 90);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(94, 184);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 90);
+            this.button4.TabIndex = 0;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 473);
+            this.Controls.Add(this.btnstratgame);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnstratgame;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle
+{
+    public partial class Form1 : Form
+    {
+        Point EmptyPoint;
+        ArrayList images = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+            EmptyPoint.X = 180;
+            EmptyPoint.Y = 180;
+        }
+
+        private void btnstratgame_Click(object sender, EventArgs e)
+        {
+            foreach (Button b  in panel1.Controls)
+            
+                b.Enabled = true;
+                Image original = Image.FromFile(@"C:\Users\Public\Pictures\Sample Pictures\Koala.jpg");
+
+                CropImageToImages(original, 270, 270);
+                AddImagesToButtons(images);
+
+            
+        }
+
+        private void AddImagesToButtons(ArrayList images)
+        {
+            int i=0;
+            int [] arr={0,1,2,3,4,5,6,7};
+            arr=Suffle(arr);
+            foreach (Button b in panel1.Controls)
+            {
+                if (i < arr.Length)
+                {
+                    b.Image = (Image)images[arr[i]];
+                    i++;
+                }
+           
+            }
+
+        }
+
+        private int[] Suffle(int[] arr)
+        {
+            Random rand = new Random();
+            arr = arr.OrderBy(x => rand.Next()).ToArray();
+            return arr;
+        }
+
+        private void CropImageToImages(Image original, int W, int H)
+        {
+            Bitmap bmp = new Bitmap(W, H);
+            Graphics graphic = Graphics.FromImage(bmp);
+            graphic.DrawImage(original, 0, 0, W, H);
+            graphic.Dispose();
+            int mov_r = 0, mov_d = 0;  
+            for (int x = 0; x < 8; x++)
+            {
+                Bitmap piece = new Bitmap(90, 90);
+                for (int i = 0; i < 90; i++)
+                
+                    for (int j = 0; j < 90; j++)
+
+                        piece.SetPixel(i, j, bmp.GetPixel(i + mov_r, j + mov_d));
+                images.Add(piece);
+                mov_r += 90;
+                if (mov_r == 270)
+                {
+                    mov_r = 0;
+                    mov_d += 90;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    
+
+    }
+}
